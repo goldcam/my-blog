0937a960292d00b7f30c229962d9719bbbeab5b7
Cameron Townsend <cameron.townsend@CTOWNSEND-MAC.local>
2019-10-11 15:27:03 -0700

    first commit

---
+++ src/build/precache-manifest.81b4c47d28f1ab4deab3af1d410283f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c07c478a572477d7726df5790c84bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3862e2326adfa4b0e17a",
+    "url": "/static/css/main.c58b48e1.chunk.css"
+  },
+  {
+    "revision": "01b8cb403a0384b66b4c",
+    "url": "/static/js/2.aa2cecaf.chunk.js"
+  },
+  {
+    "revision": "3862e2326adfa4b0e17a",
+    "url": "/static/js/main.561615ad.chunk.js"
+  },
+  {
+    "revision": "fe493dce2f263a4a22bd",
+    "url": "/static/js/runtime-main.65d1ecb9.js"
+  }
+]);
